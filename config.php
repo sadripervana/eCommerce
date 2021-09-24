d5261f4044333e4d92dd0721ce8c06a02fac3968
sadripervana <sadripervana@gmail.com>
2021-09-24 18:15:56 +0200

    new

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
 define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/PHPProjects/PHPeCommerce1/');
-define('CART_COOKIE', 'SBF552SASR2asf5as');
-define('CART_COOKIE_EXPIRE', time() + (86540* 30));
+define('CART_COOKIE','SwHbNDP3dmIX8P0XB');
+define('CART_COOKIE_EXPIRE', time() + (86400 * 30));
 ?>

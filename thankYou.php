1af93af897b44f6b9b01d865fc0ee750ff063bc7
sadripervana <sadripervana@gmail.com>
2021-09-29 13:33:22 +0200

    update db after payment

--- thankYou.php
+++ thankYou.php
@@ -1,5 +1,10 @@
 <?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'core/init.php';
+
 
 
 // Set your secret key: remember to change this to your live secret key in production
@@ -8,7 +13,8 @@ require_once 'core/init.php';
 
 // Get the credit card details submitted by the form
 $token = $_POST['stripeToken'];
-// GET the rest of the post data
+
+// Get the rest of the post data
 $full_name = sanitize($_POST['full_name']);
 $email = sanitize($_POST['email']);
 $street = sanitize($_POST['street']);
@@ -23,11 +29,16 @@ $grand_total = sanitize($_POST['grand_total']);
 $cart_id = sanitize($_POST['cart_id']);
 $description = sanitize($_POST['description']);
 $charge_amount = number_format((int)$grand_total,2)*100;
-$metadata = [
-	"cart_id" => $cart_id,
-	"tax" => $tax,
+$metadata = array(
+	"cart_id" 	=> $cart_id,
+	"tax" 		=> $tax,
 	"sub_total" => $sub_total,
-];
+);
+
+
+
+
+// Create the charge on Stripe's servers - this will charge the user's card
 try {
 	$charge = \Stripe\Charge::create(array(
 		"amount" => $charge_amount, //amount in cents, again
@@ -37,34 +48,64 @@ try {
 		"receipt_email" => $email,
 		"metadata" => $metadata)
 );
-$db->query("UPDATE cart SET paid = 1 WHERE id ='{$cart_id}'");
+
+//adjust inventory
+$itemQ = $db->query("SELECT * FROM cart WHERE id = '{$cart_id}'");
+$iresults = mysqli_fetch_assoc($itemQ);
+$items = json_decode($iresults['items'],true);
+foreach ($items as $item) {
+	$newSizes = array();
+	$item_id = $item['id'];
+	$productQ = $db->query("SELECT sizes FROM products WHERE id = '{$item_id}'");
+	$product = mysqli_fetch_assoc($productQ);
+	$sizes = sizesToArray($product['sizes']);
+	foreach($sizes as $size){
+		if($size['size'] == $item['size']){
+			$q = $size['quantity'] - $item['quantity'];
+			$newSizes[] = array('size' => $size['size'], 'quantity' => $q);
+		} else {
+			$newSizes[] = array('size' => $size['size'], 'quantity' => $size['quantity']);
+		}
+	}
+	$sizeString = sizesToString($newSizes);
+	$db->query("UPDATE products SET sizes = '{$sizeString}' WHERE id = '{$item_id}'");
+}
+
+
+//update cart
+$db->query("UPDATE cart SET paid = 1 WHERE id = '{$cart_id}'");
 $db->query("INSERT INTO transactions 
 	(charge_id,cart_id,full_name,email,street,street2,city,state,zip_code,country,sub_total,tax,grand_total,description,txn_type) VALUES 
 	('$charge->id','$cart_id','$full_name','$email','$street','$street2','$city','$state','$zip_code','$country','$sub_total','$tax','$grand_total','$description','$charge->object')");
 $domain = (($_SERVER['HTTP_HOST'] != 'localhost')?'.'.$_SERVER['HTTP_HOST']:false);
 setcookie(CART_COOKIE,'',1,"/",$domain,false);
+
 include 'includes/head.php';
 include 'includes/navigation.php';
 include 'includes/headerpartial.php';
+
 ?>
-<h1 class="text-center text-success">Thank You!</h1>
-<p>
-	Your card has been successfully charged <?=money($grand_total);?>. You have been emailed a receipt.
-	Please check your spam folder if it is not in your inbox. Additionally you can print this page as a receipt
-</p>
-<p>Your receipt number is: <strong><?=$cart_id;?></strong></p>
-<p>Your order will be shipped to the address below</p>
-<address>
-	<?=$full_name;?><br>
-	<?=$street;?><br>
-	<?=(($street2 !='')?$street2.'<br>':'');?><br>
-	<?=$city.', '.$state.', '.$zip_code;?><br>
-	<?=$country;?><br>
-</address>
+
+	<h1 class="text-center text-success">Thank You!</h1>
+	<p> Your card has been successfully charged <?=money($grand_total);?>. You have been emailed a receipt. Please check your spam folder if it's not in your inbox. Additionally you can print this page as a receipt. </p>
+	<p>Your receipt number is: <strong><?=$cart_id;?></strong></p>
+	<p>Your order will be shipped to the address below.</p>
+	<address>
+		<?=$full_name;?><br>
+		<?=$street;?><br>
+		<?=(($street2 != '')?$street2.'<br>':'');?>
+		<?=$city.', '.$state.' '.$zip_code;?><br>
+		<?=$country;?><br>
+	</address>
+
+
+
 <?php
 include 'includes/footer.php';
+
 } catch(\Stripe\Error\Card $e) {
 	// The card has been declined
 	echo $e;
 } 
+
 ?>

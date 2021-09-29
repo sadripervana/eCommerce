461b3da7852f953e136ed72cf7a06fc16c74d162
sadripervana <sadripervana@gmail.com>
2021-09-29 16:52:13 +0200

    stilizim

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,15 +1,19 @@
-<?php
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL); 
+
 function display_errors($errors){
-	$display = '<ul class="bg-danger">';
-	foreach ($errors as $error) {
-		$display .= '<li class="text-danger">'. $error . '</li>';
+	$display = '<ul style="margin-top: 60px;" class="bg-danger">';
+	foreach($errors as $error){
+		$display .= '<li class="text-danger">'.$error.'</li>';
 	}
 	$display .= '</ul>';
-	return $display;
+	return $display; 	
 }
 
-function sanitize($dirty){
-	return htmlentities($dirty, ENT_QUOTES, "UTF-8"); 
+function sanitize($dirty) {
+	return htmlentities($dirty, ENT_QUOTES, "UTF-8");
 }
 
 function money($number){
@@ -28,62 +32,65 @@ function login($user_id){
 function is_loged_in(){
 	if(isset($_SESSION['SBUser']) && $_SESSION['SBUser'] > 0){
 		return true;
-	} else
+	}
 	return false;
 }
 
 function login_error_redirect($url = 'login.php'){
-	$_SESSION['error_flash'] = 'You must be logged in to acces that page!';
+	$_SESSION['error_flash'] = 'You must be logged in to access that page.';
+	header('Location: '.$url);
+}
+
+function permission_error_redirect($url = 'login.php'){
+	$_SESSION['error_flash'] = 'You do not have permission to access that page.';
 	header('Location: '.$url);
 }
 
 function has_permission($permission = 'admin'){
 	global $user_data;
 	$permissions = explode(',', $user_data['permissions']);
-	if(in_array($permission, $permissions, true)){
+	if(in_array($permission, $permissions,true)){
 		return true;
-	} else 
-	return false;
+	}
+	return false; 
 }
 
-function permission_error_redirect($url = 'login.php'){
-	$_SESSION['error_flash'] = 'You do not have permission to acces that page!';
-	header('Location: '.$url);
-}
-
-function pretty_date($date){
-	return date("M d, Y h:i A",strtotime($date));
+function prettyDate($date){
+	return date("M d, Y h:i A", strtotime($date));
 }
 
 function get_category($child_id){
-	global $db ;
+	global $db;
 	$id = sanitize($child_id);
-	$sql = "SELECT p.id AS 'pid', p.category as 'parent', c.id AS 'cid', c.category AS 'child'
-	FROM categories c 
-	INNER JOIN categories p 
-	ON c.parent = p.id
-	WHERE c.id = '$id'";
+	$sql = "SELECT p.id AS 'pid', p.category AS 'parent', c.id AS 'cid', c.category AS 'child'
+			FROM categories c
+			INNER JOIN categories p
+			ON c.parent = p.id
+			WHERE c.id = '$id'";
 	$query = $db->query($sql);
 	$category = mysqli_fetch_assoc($query);
 	return $category;
 }
 
+
 function sizesToArray($string){
 	$sizesArray = explode(',',$string);
-	$returnArray = [];
-	foreach ($sizesArray as $size) {
+	$returnArray = array();
+	foreach($sizesArray as $size){
 		$s = explode(':',$size);
-		$returnArray[] = ['size' => $s[0], 'quantity' => $s[1]];
+		$returnArray[] = array('size' => $s[0], 'quantity' => $s[1]);
 	}
 	return $returnArray;
 }
 
+
 function sizesToString($sizes){
 	$sizeString = '';
-	foreach ($sizes as $size) {
-		$sizeString .= $size['size'] . ':'.$size['quantity'].',';
+	foreach($sizes as $size){
+		$sizeString .= $size['size'].':'.$size['quantity'].',';
 	}
-	$trimed = rtrim($sizeString, ',');
-	return $trimed;
+	$trimmed = rtrim($sizeString, ',');
+	return $trimmed;
 }
-?>
+
+ ?>

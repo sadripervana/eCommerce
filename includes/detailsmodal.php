46a4ce6e35a4992957a787dd9c567fb48d396fd3
sadripervana <sadripervana@gmail.com>
2021-09-29 13:31:20 +0200

    hide when no available sizes

--- includes/detailsmodal.php
+++ includes/detailsmodal.php
@@ -33,7 +33,7 @@ $size_array = explode(',', $sizestring);
 						<span id="modal_errors" class="bg-danger"></span>
 						<div class="col-sm-6">
 							<div class="center-block">
-								<img src="<?=$product['images']; ?>" alt="<?= $product['title']; ?>" class="details img-responsive">
+								<img src="<?=$product['image']; ?>" alt="<?= $product['title']; ?>" class="details img-responsive">
 							</div>
 						</div>
 						<div class="col-sm-6">
@@ -62,7 +62,9 @@ $size_array = explode(',', $sizestring);
 											$string_array= explode(':', $string);
 											$size = $string_array[0];
 											$available = $string_array[1];
-											echo '<option value="' . $size . '"data-available="' . $available . '">' . $size . ' (' . $available. ' Available)</option>';
+											if($available > 0){
+												echo '<option value="' . $size . '"data-available="' . $available . '">' . $size . ' (' . $available. ' Available)</option>';
+											}
 										};
 										?>
 									</select>

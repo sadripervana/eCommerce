87fa7ca56e029f2592a0b3ac49b43bec232650c7
sadripervana <sadripervana@gmail.com>
2021-09-30 13:01:53 +0200

    deleted old code comment

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -25,23 +25,6 @@
       jQuery('#sizes').val(sizeString);
     }
 
-    // function get_child_options(){
-    //   var parentID = jQuery('#parent').val();
-    //     jQuery.ajax({
-    //       url: '/PHPProjects/PHPeCommerce1/admin/parsers/child_categories.php',
-    //       type: 'POST',
-    //       data: {parentID :parentID},
-    //       success: function(data){
-    //         jQuery('#child').html(data);
-    //       },
-    //       error: function(){alert("Something went wrong with the child options.");}
-
-    //     })
-    // }
-    // jQuery('select[name="parent"]').change(get_child_options);
-
-
-
     function get_child_options(selected){
     if(typeof selected === 'undefined'){
       var selected = '';

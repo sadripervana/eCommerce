c5f344553d53579b00b9846d32001595e6cf7b90
sadripervana <sadripervana@gmail.com>
2021-10-01 16:23:31 +0200

    Shtuar administator option

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -4,6 +4,7 @@
 		<ul class="nav navbar-nav">
 
 			<!-- Menu Items-->
+			<li><a href="index.php">Administation</a></li>			
 			<li><a href="brands.php">Brands</a></li>			
 			<li><a href="categories.php">Categories</a></li>
 			<li><a href="products.php">Products</a></li>

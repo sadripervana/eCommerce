22a82dd3f1405f13c1b5814afa7e067fdd7baef3
sadripervana <sadripervana@gmail.com>
2021-10-02 13:43:34 +0200

    new link

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-default navbar-fixed-top">
 	<div class="container">
-		<a href="/PHPProjects/PHPeCommerce1/index.php" class="navbar-brand">SuperMario Toys Admin</a>
+		<a href="<?=BASEURL; ?>index.php" class="navbar-brand">SuperMario Toys Admin</a>
 		<ul class="nav navbar-nav">
 
 			<!-- Menu Items-->
-			<li><a href="brands.php">Brands</a></li>			
+			<li><a href="brands.php">Brands</a></li>
 			<li><a href="categories.php">Categories</a></li>
 			<li><a href="products.php">Products</a></li>
 			<li><a href="archived.php">Archived</a></li>
@@ -12,7 +12,7 @@
 			<li><a href="users.php">Users</a></li>
 			<?php endif; ?>
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hello <?=$user_data['first']; ?>! 
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hello <?=$user_data['first']; ?>!
 					<span class="caret"></span></a>
 				<ul class="dropdown-menu" role="menu">
 					<li><a href="change_password.php">Change Password</a></li>
@@ -24,9 +24,9 @@
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?>
 				 <span class="caret"></span></a>
 				<ul class="dropdown-menu" role="menu">
-					
+
 					<li><a href="#"></a></li>
-				
+
 				</ul>
 			</li> -->
 		</ul>

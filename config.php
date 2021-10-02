f47e11de5bcc41d3dc3441625742fb07a3d824b0
sadripervana <sadripervana@gmail.com>
2021-10-02 13:45:32 +0200

    new baseline

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
-<?php 
-define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/PHPProjects/PHPeCommerce1/');
+<?php
+// define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'eCommerce/');
+
 define('CART_COOKIE','SwHbNDP3dmIX8P0XB');
 define('CART_COOKIE_EXPIRE', time() + (86400 * 30));
 ?>

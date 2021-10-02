1a37e8cb10bf11b5337145a7799d8deacffba336
sadripervana <sadripervana@gmail.com>
2021-10-02 13:55:27 +0200

    bug fix

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'eCommerce/');
+define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/eCommerce/');
 
 define('CART_COOKIE','SwHbNDP3dmIX8P0XB');
 define('CART_COOKIE_EXPIRE', time() + (86400 * 30));

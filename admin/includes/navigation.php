40f9d5a22126caaf8154601ae7daa7c7480f9737
sadripervana <sadripervana@gmail.com>
2021-10-05 12:51:56 +0200

    added icons

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -4,9 +4,8 @@
 		<ul class="nav navbar-nav">
 
 			<!-- Menu Items-->
-			<li><a href="brands.php">Brands</a></li>
-			<li><a href="index.php">Administation</a></li>			
-			<li><a href="brands.php">Brands</a></li>			
+			<li><a href="index.php">My Dashboard</a></li>			
+			<li><a href="brands.php">Brands</a></li>		
 			<li><a href="categories.php">Categories</a></li>
 			<li><a href="products.php">Products</a></li>
 			<li><a href="archived.php">Archived</a></li>
@@ -14,11 +13,11 @@
 			<li><a href="users.php">Users</a></li>
 			<?php endif; ?>
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hello <?=$user_data['first']; ?>!
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fas fa-user"></i> Hello <?=$user_data['first']; ?>!
 					<span class="caret"></span></a>
 				<ul class="dropdown-menu" role="menu">
-					<li><a href="change_password.php">Change Password</a></li>
-					<li><a href="logout.php">Log Out</a></li>
+					<li><a href="change_password.php"><i class="fas fa-key"></i> Change Password</a></li>
+					<li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Log Out</a></li>
 				</ul>
 			</li>
 
